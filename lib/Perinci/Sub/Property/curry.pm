50f9be0fc0e31c95ac69a909c87927779bc79ae9
Steven Haryanto <stevenharyanto@gmail.com>
2012-08-09 05:54:10 +0000

    Import of SHARYANTO/Perinci-Sub-Property-curry-0.03 from CPAN.
    gitpan-cpan-distribution: Perinci-Sub-Property-curry
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SHARYANTO/Perinci-Sub-Property-curry-0.03.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Perinci/Sub/Property/curry.pm
+++ lib/Perinci/Sub/Property/curry.pm
@@ -1,4 +1,4 @@
-package Perinci::Sub::property::curry;
+package Perinci::Sub::Property::curry;
 
 use 5.010;
 use strict;
@@ -7,7 +7,7 @@ use warnings;
 
 use Perinci::Util qw(declare_property);
 
-# VERSION
+our $VERSION = '0.03'; # VERSION
 
 declare_property(
     name => 'curry',
@@ -15,6 +15,7 @@ declare_property(
     schema => ['hash*'],
     wrapper => {
         meta => {
+            v       => 2,
             prio    => 10,
             convert => 1,
         },
@@ -39,6 +40,18 @@ declare_property(
 1;
 # ABSTRACT: Set arguments for function
 
+
+__END__
+=pod
+
+=head1 NAME
+
+Perinci::Sub::Property::curry - Set arguments for function
+
+=head1 VERSION
+
+version 0.03
+
 =head1 SYNOPSIS
 
  # in function metadata
@@ -50,14 +63,34 @@ declare_property(
  f(b=>20, c=>30); # equivalent to f(a=>10, b=>20, c=>30)
  f(a=>5, b=>20);  # error, a has been set by curry
 
-
 =head1 DESCRIPTION
 
 This property sets arguments for function.
-
 
 =head1 SEE ALSO
 
 L<Perinci>
 
+=head1 DESCRIPTION
+
+
+This module has L<Rinci> metadata.
+
+=head1 FUNCTIONS
+
+
+None are exported by default, but they are exportable.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
 =cut
+
